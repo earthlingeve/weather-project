e0570b2fb736dd3b392be10fd249e71854c80752
earthlingeve <ealanis1994@gmail.com>
2022-08-14 08:04:51 -0500

    Create app.js
    pasted my js code

---
+++ src/app.js
@@ -0,0 +1,81 @@
+let h3 = document.querySelector("h3");
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Satruday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+h3.innerHTML = `${day} ${hours}:${minutes}`;
+
+function isSearch(event) {
+  event.preventDefault();
+  let input = document.querySelector("#search-city");
+  let cityName = `${input.value}`;
+  let apiKey = "9251248a80cd4432d5256905f9a0ea2f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function showTemp(response) {
+  // console.log(response.data);
+  let temperature = Math.round(response.data.main.temp);
+  let currentTemp = document.querySelector("#main-temp");
+  currentTemp.innerHTML = `${temperature}`;
+  let h2 = document.querySelector("#current-city");
+  h2.innerHTML = response.data.name;
+}
+function giveCurrentLocation(response) {
+  // console.log(response);
+  let h2 = document.querySelector("#current-city");
+  h2.innerHTML = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let currentTemp = document.querySelector("#main-temp");
+  currentTemp.innerHTML = `${temperature}`;
+}
+function showCurrentLocation(position) {
+  let latt = position.coords.latitude;
+  let lonn = position.coords.longitude;
+  console.log(position);
+  let apiKey = "9251248a80cd4432d5256905f9a0ea2f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latt}&lon=${lonn}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(giveCurrentLocation);
+}
+function currentPosition() {
+  navigator.geolocation.getCurrentPosition(showCurrentLocation);
+}
+
+let form = document.querySelector("#search-bar");
+form.addEventListener("submit", isSearch);
+
+let locButton = document.querySelector("#location-button");
+locButton.addEventListener("click", currentPosition);
+
+//function changeMeasurementC(event) {
+//event.preventDefault();
+//let mainTemp = document.querySelector("#main-temp");
+//mainTemp.innerHTML = "28";
+//}
+//function changeMeasurementF(event) {
+//event.preventDefault();
+//let mainTemp = document.querySelector("#main-temp");
+//mainTemp.innerHTML = "83";
+//}
+
+//let farenheit = document.querySelector("#farenheit");
+//let celsius = document.querySelector("#celsius");
+
+//celsius.addEventListener("click", changeMeasurementC);
+//farenheit.addEventListener("click", changeMeasurementF);
