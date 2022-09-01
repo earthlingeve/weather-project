91d5dcbddfd7d2e8e0cf23f26a05fee2deba7c77
earthlingeve <ealanis1994@gmail.com>
2022-09-01 12:41:30 -0500

    added onecall api for forecast
    incorporated getForecast function with showTemp function.

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Fri", "Sat", "Sun"];
   let forecastHTML = `<div class="row">`;
@@ -60,6 +61,13 @@ function isSearch(event) {
   axios.get(apiUrl).then(showTemp);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "9251248a80cd4432d5256905f9a0ea2f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showForecast);
+}
+
 function showTemp(response) {
   let descriptionElement = document.querySelector("#description-element");
   let time = document.querySelector("#time");
@@ -87,6 +95,8 @@ function showTemp(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 function giveCurrentLocation(response) {
   console.log(response);
@@ -158,4 +168,3 @@ celsius.addEventListener("click", changeMeasurementC);
 fahrenheit.addEventListener("click", changeMeasurementF);
 
 currentPosition();
-showForecast();
